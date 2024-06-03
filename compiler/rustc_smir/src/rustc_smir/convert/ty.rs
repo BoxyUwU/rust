592d43fc98a6b9427a24fac080b173ab7fb0159d
Boxy <supbscripter@gmail.com>
2024-06-03 01:10:24 +0100

    Basic removal of `Ty` from places (boring)

--- compiler/rustc_smir/src/rustc_smir/convert/ty.rs
+++ compiler/rustc_smir/src/rustc_smir/convert/ty.rs
@@ -422,7 +422,10 @@ impl<'tcx> Stable<'tcx> for ty::Const<'tcx> {
                         ty::ValTree::Branch(tables.tcx.lift(branch).unwrap())
                     }
                 };
-                let ty = tables.tcx.lift(self.ty()).unwrap();
+
+                // THISPR
+                let ty = todo!();
+                // let ty = tables.tcx.lift(c.ty()).unwrap();
                 let const_val = tables.tcx.valtree_to_const_val((ty, val));
                 if matches!(const_val, mir::ConstValue::ZeroSized) {
                     ConstantKind::ZeroSized
@@ -446,7 +449,9 @@ impl<'tcx> Stable<'tcx> for ty::Const<'tcx> {
             }
             ty::ExprCt(_) => unimplemented!(),
         };
-        let ty = self.ty().stable(tables);
+        // THISPR
+        let ty = todo!();
+        // let ty = self.ty().stable(tables);
         let id = tables.intern_const(mir::Const::Ty(tables.tcx.lift(*self).unwrap()));
         Const::new(kind, ty, id)
     }
