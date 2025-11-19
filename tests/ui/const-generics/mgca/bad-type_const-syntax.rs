7273d2d939c45def9fe3ea93d69c26c57d0954fd
Boxy Uwu <rust@boxyuwu.dev>
2025-11-19 18:32:49 +0000

    fix tests

--- tests/ui/const-generics/mgca/bad-type_const-syntax.rs
+++ tests/ui/const-generics/mgca/bad-type_const-syntax.rs
@@ -11,6 +11,7 @@ impl Tr for S {
     #[type_const]
     //~^ ERROR experimental
     const N: usize = const { 0 };
+    //~^ ERROR: unbraced const blocks as const args are experimental
 }
 
 fn main() {}
