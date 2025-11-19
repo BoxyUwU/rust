64ae90e5c376312a91eacfa148bdc2cd0c18be31
Boxy Uwu <rust@boxyuwu.dev>
2025-11-19 18:32:57 +0000

    format the world

--- compiler/rustc_type_ir/src/relate.rs
+++ compiler/rustc_type_ir/src/relate.rs
@@ -601,11 +601,16 @@ pub fn structurally_relate_consts<I: Interner, R: TypeRelation<I>>(
         (ty::ConstKind::Placeholder(p1), ty::ConstKind::Placeholder(p2)) => p1 == p2,
         (ty::ConstKind::Value(a_val), ty::ConstKind::Value(b_val)) => {
             match (a_val.valtree().kind(), b_val.valtree().kind()) {
-                (ty::ValTreeKind::Leaf(scalar_a), ty::ValTreeKind::Leaf(scalar_b)) => scalar_a == scalar_b,
+                (ty::ValTreeKind::Leaf(scalar_a), ty::ValTreeKind::Leaf(scalar_b)) => {
+                    scalar_a == scalar_b
+                }
                 (ty::ValTreeKind::Branch(branches_a), ty::ValTreeKind::Branch(branches_b))
                     if branches_a.len() == branches_b.len() =>
                 {
-                    branches_a.into_iter().zip(branches_b).all(|(a, b)| relation.relate(*a, *b).is_ok())
+                    branches_a
+                        .into_iter()
+                        .zip(branches_b)
+                        .all(|(a, b)| relation.relate(*a, *b).is_ok())
                 }
                 _ => false,
             }
